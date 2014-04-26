d2eca5a3f40319004f3ddb12060fe950d04c858a
abc8257595 <abc8257595@gmail.com>
2014-04-26 18:39:43 +0800

    删除了一些没用的头

--- LaneDetect.cpp
+++ LaneDetect.cpp
@@ -1,24 +1,4 @@
 /*------------------------------------------------------------------------------------------*\
-   Lane Detection
-
-   General idea and some code modified from:
-   chapter 7 of Computer Vision Programming using the OpenCV Library. 
-   by Robert Laganiere, Packt Publishing, 2011.
-
-   This program is free software; permission is hereby granted to use, copy, modify, 
-   and distribute this source code, or portions thereof, for any purpose, without fee, 
-   subject to the restriction that the copyright notice may not be removed 
-   or altered from any source or altered source distribution. 
-   The software is released on an as-is basis and without any warranties of any kind. 
-   In particular, the software is not guaranteed to be fault-tolerant or free from failure. 
-   The author disclaims all warranties with regard to this software, any use, 
-   and any consequent failure, is purely the responsibility of the user.
- 
-   Copyright (C) 2013 Jason Dorweiler, www.transistor.io
-
-
-Notes: 
-
 	Add up number on lines that are found within a threshold of a given rho,theta and 
 	use that to determine a score.  Only lines with a good enough score are kept. 
 
@@ -36,9 +16,9 @@ Notes:
 	Added image ROI to reduce false lines from things like trees/powerlines
 \*------------------------------------------------------------------------------------------*/
 
-#include "opencv2/highgui/highgui.hpp"
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <highgui/highgui.hpp>
+#include <objdetect/objdetect.hpp>
+#include <imgproc/imgproc.hpp>
 #include <iostream>
 #include <vector>
 #include <stdio.h>
--- linefinder.h
+++ linefinder.h
@@ -1,27 +1,9 @@
-/*------------------------------------------------------------------------------------------*\
-   Lane Detection
-
-   General idea and some code modified from:
-   chapter 7 of Computer Vision Programming using the OpenCV Library. 
-   by Robert Laganiere, Packt Publishing, 2011.
-
-   This program is free software; permission is hereby granted to use, copy, modify, 
-   and distribute this source code, or portions thereof, for any purpose, without fee, 
-   subject to the restriction that the copyright notice may not be removed 
-   or altered from any source or altered source distribution. 
-   The software is released on an as-is basis and without any warranties of any kind. 
-   In particular, the software is not guaranteed to be fault-tolerant or free from failure. 
-   The author disclaims all warranties with regard to this software, any use, 
-   and any consequent failure, is purely the responsibility of the user.
- 
-   Copyright (C) 2013 Jason Dorweiler, www.transistor.io
-\*------------------------------------------------------------------------------------------*/
-
 #if !defined LINEF
 #define LINEF
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <core/core.hpp>
+#include <imgproc/imgproc.hpp>
+
 #define PI 3.1415926
 
 class LineFinder {
